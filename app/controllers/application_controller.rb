6532c9db9f2b6cc145d4c4230130aabf687bcbd9
w0www <imanol@alvarezperez.net>
2016-09-22 09:51:53 +0200

    cambios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+  include Hobo::Controller::AuthenticationSupport
+  before_filter :except => [:login, :forgot_password, :reset_password, :do_reset_password] do
+   login_required unless User.count == 0
+  end
+
 end
--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,6 +5,10 @@
 Saunierduval::Application.routes.draw do
 
 
+  # Resource routes for controller notificacions
+  resources :notificacions
+
+
   # Resource routes for controller users
   resources :users, :only => [:edit, :show, :create, :update, :destroy] do
     collection do
@@ -24,10 +28,6 @@ Saunierduval::Application.routes.draw do
   get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
   get 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
   post 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password_post'
-
-
-  # Resource routes for controller notificacions
-  resources :notificacions
 
 
   # Resource routes for controller financiacions
