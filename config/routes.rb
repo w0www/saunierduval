469a26e821cbc07f4ce432474d87be2a805110a3
w0www <imanol@alvarezperez.net>
2016-09-22 10:40:19 +0200

    cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get ENV['RAILS_RELATIVE_URL_ROOT'] => 'front#index' if ENV['RAILS_RELATIVE_URL_ROOT']
-  root :to => 'front#index'
+  root :to => 'financiacions#index'
   get 'users/:id/reset_password_from_email/:key' => 'users#reset_password', :as => 'reset_password_from_email'
   get 'users/:id/accept_invitation_from_email/:key' => 'users#accept_invitation', :as => 'accept_invitation_from_email'
   get 'users/:id/activate_from_email/:key' => 'users#activate', :as => 'activate_from_email'
